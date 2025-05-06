9839a5045f1ff9ee1cfda56656d3df4ab8524623
Taha Musani <taha@softhealer.com>
2025-05-06 10:51:16 +0530

    Delete sh_logo_replace directory

--- sh_logo_replace/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pos_config
-from . import res_config_setting
-from . import product
--- sh_logo_replace/models/res_config_setting.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    pos_enable_pos_receipt_logo = fields.Boolean(related="pos_config_id.enable_pos_receipt_logo",readonly=False)
-    pos_receipt_logo = fields.Binary(related="pos_config_id.receipt_logo",readonly=False)
-    pos_enable_pos_header_logo = fields.Boolean(related="pos_config_id.enable_pos_header_logo",readonly=False)
-    pos_header_logo = fields.Binary(related="pos_config_id.header_logo",readonly=False)
