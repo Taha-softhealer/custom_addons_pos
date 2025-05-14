fb89249d18b4965fb7ef5c451059f7678e0ae9f7
Taha Musani <taha@softhealer.com>
2025-05-14 18:46:28 +0530

    [add] sh_pos_expiry_lot_restriction

---
+++ sh_pos_expiry_lot_restriction/__manifest__.py
@@ -0,0 +1,20 @@
+# Part of Softhealer Technologies.
+{
+    "name": "Point Of Sale Expiry Lot Restriction",
+    "author": "Softhealer Technologies",
+    "website": "https://www.softhealer.com",
+    "support": "support@softhealer.com",
+    "category": "Point Of Sale",
+    "license": "OPL-1",
+    "summary": "",
+    "description": """""",
+    "version": "0.0.1",
+    "depends": ["point_of_sale"],
+    "application": True,
+    "data": [],
+    'assets': {
+        'point_of_sale._assets_pos': [],
+    },
+    "auto_install": False,
+    "installable": True,
+}
