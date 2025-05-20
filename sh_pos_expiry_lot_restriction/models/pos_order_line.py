5653cc8982323774b15ec0aeb08ee83cb0df66f7
Taha Musani <taha@softhealer.com>
2025-05-20 10:52:43 +0530

    [add] solved some bugs

--- sh_pos_expiry_lot_restriction/models/pos_order_line.py
+++ sh_pos_expiry_lot_restriction/models/pos_order_line.py
@@ -9,11 +9,6 @@ class PosOrderLine(models.Model):
 
     @api.model
     def sh_get_existing_lots(self, company_id, product_id):
-        """
-        Return the lots that are still available in the given company.
-        The lot is available if its quantity in the corresponding stock_quant and pos stock location is > 0.
-        with expiry date
-        """
         self.check_access("read")
         pos_config = self.env["pos.config"].browse(self._context.get("config_id"))
         if not pos_config:
@@ -22,7 +17,7 @@ class PosOrderLine(models.Model):
         src_loc = pos_config.picking_type_id.default_location_src_id
         src_loc_quants = (
             self.sudo()
-            .env["stock.quant"]
+            .env["stock.lot"]
             .search(
                 [
                     "|",
@@ -32,6 +27,4 @@ class PosOrderLine(models.Model):
                 ]
             )
         )
-        available_lots = src_loc_quants.mapped("lot_id")
-        print('\n\n\n-----available_lots------->',available_lots)
-        return available_lots.read(["id", "name", "product_qty", "expiration_date"])
+        return src_loc_quants.read(["id", "name", "product_qty", "expiration_date"])
--- sh_pos_expiry_lot_restriction/static/src/overrides/pos_store/pos_store.js
+++ sh_pos_expiry_lot_restriction/static/src/overrides/pos_store/pos_store.js
@@ -1,5 +1,6 @@
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { patch } from "@web/core/utils/patch";
+import { AlertDialog, ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { ask } from "@point_of_sale/app/store/make_awaitable_dialog";
 
 patch(PosStore.prototype, {
@@ -21,55 +22,41 @@ patch(PosStore.prototype, {
             } catch (ex) {
                 alert("Can't load lots");
                 console.log("Collecting sh existing records fail", ex);
-            }
-            console.log(existingLots.length);
-            
+            }            
             let Currentdate = new Date();
             let daysToAdd = product.use_expiration_date ? product.alert_time : 0;
             let lotName = result.newPackLotLines[0]?.lot_name;
             let addedDate = new Date();
             let selectedLot = existingLots?.filter((lot) => lot.name == lotName);
             let expiry_value = false
+
+            if (!lotName) {
+                return result
+            }
             if (selectedLot.length>0 && selectedLot[0].expiration_date) {
                 expiry_value = selectedLot[0].expiration_date;
-            }
-            else{
-                const _confirmed = await ask(this.dialog, {
-                    title: "Expiration Alert",
-                    body: "The Lot/Serial number will expire on " + Currentdate?.toLocaleDateString(),
-                    confirmLabel: "Okay",
-                });
-                if (!_confirmed) {
-                    return
-                }
             }
             let expiry_date = expiry_value
                 ? new Date(selectedLot[0].expiration_date)
                 : "";
             let timeDifference = expiry_date - Currentdate;
             let daysDifference = timeDifference / (1000 * 3600 * 24);
-
+            
             addedDate.setDate(addedDate.getDate() + daysToAdd);
-            addedDate = addedDate;
             if (
-                daysDifference < -1 &&
+                Math.round(daysDifference) <= -1 &&
                 expiry_value &&
                 this.config.sh_restrict_lot_expiry
             ) {
-                const _confirmed = await ask(this.dialog, {
+                this.dialog.add(ConfirmationDialog, {
                     title: "Expiration Alert",
-                    body:
-                        "The Lot/Serial number has been expired on " +
-                        expiry_date?.toLocaleDateString() +
-                        " so you can't sell it ",
-                        confirmLabel: "Okay",
-
+                    body: "The " + lotName +" has been expired on " + expiry_date?.toLocaleDateString() + " so you can't sell it ",
                 });
                 return;
-            } else if (-1 < daysDifference && daysDifference < daysToAdd && expiry_value && this.config.sh_lot_expiry_warning) {
+            } else if (-1 < daysDifference && Math.round(daysDifference) <= daysToAdd && expiry_value && this.config.sh_lot_expiry_warning) {
                 const _confirmed = await ask(this.dialog, {
                     title: "Expiration Alert",
-                    body: "The Lot/Serial number will expire on " + expiry_date?.toLocaleDateString(),
+                    body: "The "+ lotName +" will expire on " + expiry_date?.toLocaleDateString(),
                     confirmLabel: "Okay",
                 });
                 if (!_confirmed) {
